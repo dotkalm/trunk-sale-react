aacdf6140d0e5dd6398c005ce9117cc6b17235d6
dotkalm <joelmholmberg@gmail.com>
2019-09-16 17:12:40 -0700

    component to add items

--- src/SignIn/index.js
+++ src/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react
 import { withRouter, NavLink } from 'react-router-dom'
 
 import { withFirebase } from '../Firebase'
